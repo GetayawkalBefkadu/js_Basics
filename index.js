5feeaebcaca827ba52d688ef1774ae31eee507f2
Getayawkal Befkadu <getayawkalbk@gmail.com>
2025-06-08 21:25:27 +0300

    constant decalaration

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 //My first javascript code!
     console.log("Hello world!");
-let name = 'Getch';
+    // variables declaration
+let name = 'Getch';// string 
 console.log(`Hello ${name}!`);
+let age = 20; // number
+console.log(`I am ${age} years old!`);
