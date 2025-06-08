0154db8c36e66131a1f583776dd311ae97e200ae
Getayawkal Befkadu <getayawkalbk@gmail.com>
2025-06-08 22:03:41 +0300

    declaring variable wit const

--- index.js
+++ index.js
@@ -3,8 +3,10 @@
     // variables declaration
 let name = 'Getch';// string 
 console.log(`Hello ${name}!`);
-let age = 20; // number
+let age = 24; // number
 console.log(`I am ${age} years old!`);
 //data types
-let isStudent = true; // boolean    
-
+let isStudent = true; // boolean  
+console.log(`Am I a student? ${isStudent}`); 
+const PI = 3.14; // constant
+console.log(`The value of PI is ${PI}`);
